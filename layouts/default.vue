4d0526a18ec312a5e4a86e56ca76d27a6dac9c7f
moh123adan <maxamedadan169@gmail.com>
2025-01-01 17:35:15 +0300

    fixing: with the icons

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,8 @@
     <v-app id="inspire">
       <v-app-bar extended>
         <v-icon>mdi-house</v-icon>
-  
         <v-app-bar-title>Application</v-app-bar-title>
-  
         <v-spacer></v-spacer>
-  
         <v-btn icon="mdi-dots-vertical">
         </v-btn>
       </v-app-bar>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module'],
+  modules: ['vuetify-nuxt-module', '@unocss/nuxt'],
   vuetify: {
     moduleOptions: {
       /* module specific options */
+      defaultIconSet: 'mdi', 
+
     },
     vuetifyOptions: {
-      /* vuetify options */
       icons: {
-        defaultSet: "unocss-mdi",
+        defaultSet: 'mdi',
+        sets: ['mdi', 'fa']
       }
     }
   }
