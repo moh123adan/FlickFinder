6afda739f3414d6a5a217ee6c6f56650156c7788
moh123adan <maxamedadan169@gmail.com>
2025-01-01 15:21:18 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app id="inspire">
+      <v-app-bar extended>
+        <v-icon>mdi-house</v-icon>
+  
+        <v-app-bar-title>Application</v-app-bar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon="mdi-dots-vertical">
+        </v-btn>
+      </v-app-bar>
+  
+      <v-main>
+       <slot />
+      </v-main>
+    </v-app>
+  </template>
+
+
+<script setup lang="ts"></script>
